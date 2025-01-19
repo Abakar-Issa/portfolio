d85d1801fe5b1aea5aa9a7fc4eb93fa95356a1e4
issa <issaabakar17@gmail.com>
2025-01-19 02:04:33 +0100

    changes for deploy

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',  // Ajout de la base URL pour GitHub Pages
   plugins: [
     vue(),
     vuetify({ autoImport: true }),
